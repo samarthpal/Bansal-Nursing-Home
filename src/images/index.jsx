a59dc8a493923f49fc554e430d23516ef73211af
Samarth kumar pal <palsamarth02@gmail.com>
2023-09-19 18:11:06 +0530

    Review link left

--- src/images/index.jsx
+++ src/images/index.jsx
@@ -2,6 +2,7 @@ import back from './illus/back.png'
 import lightning from './illus/lightning.png'
 import facebook from './logos/facebook.png'
 import instagram from './logos/instagram.png'
+import logo from './logos/logo.png'
 import logo1 from './logos/logo1.png'
 import review from './logos/review.png'
 import youtube from './logos/youtube.png'
@@ -11,6 +12,7 @@ const main = {
   lightning,
   facebook,
   instagram,
+  logo,
   logo1,
   review,
   youtube
