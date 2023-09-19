c47b022f540cf3493f1c9be1613555727369845b
Samarth kumar pal <palsamarth02@gmail.com>
2023-09-19 17:34:58 +0530

    Initial commit

---
+++ src/images/index.jsx
@@ -0,0 +1,19 @@
+import back from './illus/back.png'
+import lightning from './illus/lightning.png'
+import facebook from './logos/facebook.png'
+import instagram from './logos/instagram.png'
+import logo1 from './logos/logo1.png'
+import review from './logos/review.png'
+import youtube from './logos/youtube.png'
+
+const main = {
+  back,
+  lightning,
+  facebook,
+  instagram,
+  logo1,
+  review,
+  youtube
+}
+
+export default main
---
+++ src/pages/socialPage/index.jsx
@@ -0,0 +1,51 @@
+import s from './styles.module.scss'
+import images from 'images'
+
+const linkData = [
+  {
+    field: 'REVIEWS',
+    link: 'https://g.page/r/CaiQRsNDp72wEB0/review',
+    image: images.review
+  },
+  {
+    field: 'INSTAGRAM',
+    link: 'https://instagram.com/bansalnursinghome?igshid=MzRlODBiNWFlZA==',
+    image: images.instagram
+  },
+  {
+    field: 'FACEBOOK',
+    link: 'https://www.facebook.com/profile.php?id=100094448633050&mibextid=MKOS29',
+    image: images.facebook
+  }
+]
+
+export default function Main() {
+  return (
+    <div className={s.main}>
+      <div className={s.inner}>
+        <div className={s.logo}>
+          <img src={images.logo1} alt='' />
+        </div>
+        <div className={s.title}>Click Here For</div>
+        <div className={s.links}>
+          {linkData.map(({ field, link, image }, i) => (
+            <a href={link} key={i} className={s.link} target='_blank' rel='noreferrer'>
+              <div>
+                <img src={image} alt='' />
+              </div>
+              <div>{field}</div>
+            </a>
+          ))}
+        </div>
+        <div className={s.bottom}>
+          <div className={s.left}>
+            <img src={images.lightning} alt='' />
+          </div>
+          <div className={s.right}>
+            <img src={images.lightning} alt='' />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
